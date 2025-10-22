0b44d1524a55d1e68436df8d9a5f092950732f10
Makktu <johnmcna@gmail.com>
2025-10-22 19:28:01 +0100

    basic v1 done, prepping for v2

--- README.md
+++ README.md
@@ -12,3 +12,48 @@ Basic prototype to begin with.
 
 21 Oct 2025
 Got basic UI and random numbers working.
+
+22 Oct 2025
+version 1.0 done - game is basically working:
+
+- player starts game
+- game shows random sequence of numbers
+- player must match sequence
+- player wins if they match sequence
+- player loses if they don't match sequence
+- game ends when player loses
+- score is updated when player wins
+- difficulty escalates in the amount of numbers in the sequence & the speed of their appearance
+
+No visual UI work done yet - all runs in basic HTML elements in the browser.
+
+Next steps:
+
+- add visual UI
+- add colours to numbers
+- add difficulty levels
+- add visual timer
+- add bonus 'see again' feature the player can use to see the sequence again before they start to answer; this can be earned back by winning a set number of rounds
+- add scoring system
+- add game over screen
+- add high score system
+- add music
+- add sound effects, e.g. for correct/incorrect responses and individual numbers 0-9.
+
+Notes:
+
+- will always be single-digit numbers
+- will always start with 3 numbers
+- exact way difficulty escalates to be determined
+
+Targets:
+
+- keep it simple
+- all in one script
+- no external dependencies
+- no libraries
+- no frameworks
+
+Timeframe:
+
+- 3 days - version 2.0 done 25 Oct 2025
--- main.js
+++ main.js
@@ -2,27 +2,50 @@
 
 const playerDisplay = document.getElementById('player-display');
 const computerDisplay = document.getElementById('computer-display');
+const scoreDisplay = document.getElementById('score');
 
 let gameStarted = false;
+let gameInProgress = false;
+let checkingInput = false;
+let showingNumbers = false;
 let amountOfNumbers = 5;
+let startingTime = 1200;
+let numberChecked = 0;
+let currentLowNumber = 0;
+let currentHighNumber = 9;
+let numbers = [];
+let round = 1;
+let currentScore = 0;
 
 function playerTurn() {
   window.addEventListener('keydown', keyDownHandler);
 }
 
 function keyDownHandler(e) {
+  // player cannot input anything until number showing sequence complete
+  if (showingNumbers || e.repeat) {
+    return;
+  }
   let keyPressed = e.key;
   console.log(keyPressed);
-  if (!gameStarted && keyPressed === ' ') {
-    showMessage('game on!');
+
+  if (!gameStarted && keyPressed === ' ' && !gameInProgress) {
     gameStarted = true;
-    let numbers = [];
+    gameInProgress = true;
+    numbers = [];
     for (let i = 0; i < amountOfNumbers; i++) {
-      numbers.push(getRandomInt(1, 10));
+      numbers.push(getRandomInt(currentLowNumber, currentHighNumber));
     }
     console.log(numbers);
 
-    displayNumbers(numbers, 400);
+    displayNumbers(numbers, startingTime);
+  } else {
+    let keyPressedNumber = parseInt(keyPressed);
+    if (keyPressedNumber >= 0 && keyPressedNumber <= 9) {
+      handlePlayerInput(keyPressed, numbers);
+    } else {
+      console.log('non-number pressed');
+    }
   }
   if (keyPressed === 'q') {
     window.removeEventListener('keydown', keyDownHandler);
@@ -30,38 +53,92 @@ function keyDownHandler(e) {
   if (keyPressed === 'a') {
     console.log(getRandomInt(1, 10));
   }
-  // switch (keyPressed) {
-  //   case 'q':
-  //     window.removeEventListener('keydown', keyDownHandler);
-  //     break;
-  //   case 'a':
-  //     console.log(getRandomInt(1, 10));
-  //     break;
-  //   default:
-  //     break;
-  // }
 }
 
 function showMessage(message = 'nothing') {
   computerDisplay.innerHTML = `<p>${message}</p>`;
 }
 
-function getRandomInt(min = 1, max = 100) {
+function getRandomInt(min = 0, max = 9) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 async function displayNumbers(numbers = [], delayMs = 1000) {
+  showingNumbers = true; // disable player input for this
+  for (let a = 3; a > 0; a--) {
+    showMessage(`GET READY! Round ${round} starts in... ${a}`);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  }
+
   for (let i = 0; i < numbers.length + 1; i++) {
-    // need to resolve what happens when same numbers succeed one another
-    await new Promise((resolve) => setTimeout(resolve, delayMs));
-    if (numbers[i]) {
-      showMessage(numbers[i]);
+    // TODO need to resolve what happens when same numbers succeed one another
+    // await new Promise((resolve) => setTimeout(resolve, delayMs));
+    if (numbers[i] || numbers[i] === 0) {
+      showMessage(`<p id="the-numbers">${numbers[i]}</p>`);
+      // Add a brief pause with empty screen
+      await new Promise((resolve) => setTimeout(resolve, delayMs)); // blank time
+      showMessage(' ');
+      await new Promise((resolve) => setTimeout(resolve, 150)); // short gap
     } else {
       showMessage(' ');
     }
   }
+  gameStarted = false;
+  showingNumbers = false; // restore player input
+}
+
+function handlePlayerInput(keyPressed, numbers) {
+  if (checkingInput) {
+    return;
+  }
+  checkingInput = true;
+  console.log(keyPressed);
+  if (keyPressed == numbers[numberChecked]) {
+    console.log('🟢 CORRECT!');
+    numberChecked += 1;
+    if (numberChecked === numbers.length) {
+      showMessage('CORRECT! Press SPACE to start the next round...');
+      round += 1;
+      currentScore += 1;
+      scoreDisplay.innerHTML = `<p>Score Now: ${currentScore}</p>`;
+
+      if (round % 3 === 0) {
+        amountOfNumbers += 1;
+        if (startingTime > 399) {
+          startingTime -= 200;
+        }
+      }
+      gameStarted = false;
+      gameInProgress = false;
+      numbers = [];
+      numberChecked = 0;
+    }
+    checkingInput = false;
+    return;
+  } else {
+    console.log('🔴 WRONG!');
+    console.log(
+      'keyPressed',
+      keyPressed,
+      'numberChecked',
+      numberChecked,
+      'numbers[numberChecked',
+      numbers[numberChecked]
+    );
+    gameOver();
+  }
+}
+
+function gameOver() {
+  console.log('Game Over');
+  gameInProgress = false;
+  numbers = [];
+  numberChecked = 0;
+  checkingInput = false;
+  gameStarted = false;
+  showMessage('Press SPACE to try again');
 }
 
 playerTurn();
